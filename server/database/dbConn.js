b96a9dd684c5c6eb250a5d3b522118d799c0efb1
unknown <akash2002155@gmail.com>
2024-07-31 14:50:32 +0530

    frontend added some css remain

--- server/database/dbConn.js
+++ server/database/dbConn.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const dbConnection = () => {
   mongoose
     .connect(process.env.MONGO_URI, {
-      dbName: "MERN_JOB_SEEKING_WEBAPP",
+      dbName: "JOB_FINDING_WEBAPP",
     })
     .then(() => {
       console.log("Connected to database.");
